38086305e02a0b6cf32a482c9c0cbf582726c679
minwoojang <q860514@naver.com>
2024-12-04 15:11:40 +0900

    rebuild

--- package.py
+++ package.py
@@ -1,20 +1,20 @@
 name = "pyilmbase"
 
-version = "2.1.0"
+version = "2.3.0"
 
 description = \
     """
     IlmBase Python bindings
     """
 
-variants = [
-    ["platform-linux"]
-]
+variants = [['platform-linux', 'arch-x86_64']]
 
 requires = [
     "ilmbase-%s" % (version),
     "python",
-    "boost"
+    "boost-1.86",
+    "gcc-11",
+    "numpy"
 ]
 
 uuid = "repository.pyilmbase"
@@ -22,4 +22,4 @@ uuid = "repository.pyilmbase"
 def commands():
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib")
-    env.PYTHONPATH.append("{root}/lib/python2.7/site-packages")
+    env.PYTHONPATH.append("{root}/lib/python3.9/site-packages")
